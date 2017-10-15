1d06ccdc21a2466eb3b8ed98b8647a989282c676
Egor <egor6417@gmail.com>
2017-10-15 14:37:50 +0300

    added feminism essay and it homework

---
+++ homework.py
@@ -0,0 +1,25 @@
+kmi=0
+kma=0
+k=0
+summ=0
+mi=10000
+ma=-1
+while True:
+	a=int(input())
+	if a==0
+		break
+	else:
+		if a%7==0:
+		k=k+1
+		summ=summ+a
+		if a>ma: 
+			ma=a
+			kma=kma+1
+		if a<mi:
+			mi=a
+			kmi-kmi+1
+print(kmi)
+print(kma)
+print(ma)
+print(mi)
+print(k)
