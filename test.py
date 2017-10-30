59f132344ed48b8fd90f24dfba48fe6e4eed184e
Egor <egor6417@gmail.com>
2017-10-30 11:57:59 +0300

    a

--- test.py
+++ test.py
@@ -4,16 +4,3 @@ d=A[-1]
 for i in range (0, len(A),-1)
 	A[i]=A[i+1]
 A[len(A)-1]=d
-
-''' data=input().split(A=list(map(int,data))
-for i in range(len(A)-1):
-	if (A[i]>A[i-1])and (A[i]>A[i+1]):
-		k=k+1
-print (k)
-
-data=input().split()
-A=list(map(int,data))
-for i in range(len(A)-1):
-	if (A[i]>0 and A[i+1]>0)or (A[i]<0 and A[i+1]<0):
-		print(A[i],A[i+1])
-		break '''
